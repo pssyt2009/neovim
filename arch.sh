19385e9f3dcdd4401317b017aa5003342e8f72b1
Prabhjot Singh Sandhu <94212553+pssyt2009@users.noreply.github.com>
2023-03-18 16:45:04 +0530

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,6 +1,6 @@
 rm -rf ~/.config/nvim 
 rm -rf ~/.local/share/nvim
-sudo pacman -S neovim   
+sudo pacman -S neovim base-devel  
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 mkdir ~/.config/nvim 
