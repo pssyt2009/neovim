896703d94678ea0d07ef643cb1c9e98fc8dda7b5
Prabhjot Singh Sandhu <94212553+pssyt2009@users.noreply.github.com>
2023-03-18 16:30:33 +0530

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -2,7 +2,7 @@ rm -rf ~/.config/nvim
 rm -rf ~/.local/share/nvim/
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update
-sudo apt install neovim -y
+sudo apt install neovim build-essential -y
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 mkdir ~/.config/nvim
